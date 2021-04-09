64398a4def1db57eb51c11f050de5545eb0802d1
jxwleong <jason.xie.wei.leong@gmail.com>
2021-04-09 19:22:16 +0800

    Basic algorithm to run prime95 with settings based on local.txt and prime.txt Finally managed to get the threads log using "Edit > Copy Window"

--- p95.py
+++ p95.py
@@ -2,37 +2,63 @@ import os
 import subprocess
 import time
 
+
+import pyperclip 
 import win32con
 import win32gui
 import win32process
+import pyautogui
+import pygetwindow as gw
+
+from win32gui import FindWindow, GetWindowRect
+from ctypes import *
+
+def remove_if_exists(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
-P95_EXE_PATH = os.path.join(ROOT_DIR, 'p95v303b6.win32', 'prime95.exe')
+P95_EXE_PATH = os.path.join(ROOT_DIR, "p95v303b6.win32", "prime95.exe")
+P95_RESULTS_TXT = os.path.join(ROOT_DIR, "p95v303b6.win32", "results.txt")
+
+remove_if_exists(P95_RESULTS_TXT)
 
 p95_process = subprocess.Popen([P95_EXE_PATH, '-t'])
+time.sleep(1)
+
+edit_x = 60
+edit_y = 40
+
+copy_x = 60
+copy_y = 65
+
+window_x = 30
+window_y = 280
 
 
-
-
-# http://timgolden.me.uk/python/win32_how_do_i/find-the-window-for-my-subprocess.html
-def get_hwnds_for_pid(pid):
-  def callback (hwnd, hwnds):
-    if win32gui.IsWindowVisible (hwnd) and win32gui.IsWindowEnabled (hwnd):
-      _, found_pid = win32process.GetWindowThreadProcessId (hwnd)
-      if found_pid == pid:
-        hwnds.append (hwnd)
-    return True
-    
-  hwnds = []
-  win32gui.EnumWindows (callback, hwnds)
-  return hwnds
-
-time.sleep(5)
-hwnd = get_hwnds_for_pid(p95_process.pid)
-print(hwnd)
-time.sleep(5)
-win32gui.ShowWindow(hwnd[0], win32con.SW_MAXIMIZE)
-time.sleep(5)
+# FindWindow takes the Window Class name (can be None if unknown), and the window's display text. 
+window_handle = FindWindow(None, "Prime95")
+window_rect   = GetWindowRect(window_handle)
+print(window_handle)
+print(window_rect)
+x, y, _, _ = window_rect
 
 time.sleep(5)
+ok = windll.user32.BlockInput(True) #enable block
+win32gui.SetForegroundWindow(window_handle)
+pyautogui.click(x=x+window_x, y=y+window_y)
+pyautogui.click(x=x+edit_x, y=y+edit_y)
+pyautogui.click(x=x+copy_x, y=y+copy_y)
+ok = windll.user32.BlockInput(False) #disable block 
+
 subprocess.Popen(f"taskkill /PID {p95_process.pid}")
+
+
+p95_log_str = pyperclip.paste()
+with open(os.path.join(os.getcwd(), "prime95.log"), "w") as p95_log:
+  p95_log_str = p95_log_str.replace("\n", "")
+  p95_log.write(p95_log_str)
+
+
+print(ok)
