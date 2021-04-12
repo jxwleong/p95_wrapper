efdec6d9694fff48c65d7966f392b139ae0f7166
jxwleong <jason.xie.wei.leong@gmail.com>
2021-04-12 20:09:49 +0800

    Added script to move results.txt to cwd

--- p95.py
+++ p95.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 import time
+import shutil
 
 from ctypes import *
 
@@ -19,17 +20,18 @@ from win32gui import FindWindow, GetWindowRect
 
 
 P95_EXE_PATH = os.path.join(ROOT_DIR, "p95v303b6.win32", "prime95.exe")
-P95_RESULTS_TXT = os.path.join(ROOT_DIR, "p95v303b6.win32", "results.txt")
-P95_LOCAL_TXT = os.path.join(ROOT_DIR, "p95v303b6.win32", "local.txt")
-P95_PRIME_TXT = os.path.join(ROOT_DIR, "p95v303b6.win32", "prime.txt")
-
+P95_RESULTS_TXT_PATH = os.path.join(ROOT_DIR, "p95v303b6.win32", "results.txt")
+P95_LOCAL_TXT_PATH = os.path.join(ROOT_DIR, "p95v303b6.win32", "local.txt")
+P95_PRIME_TXT_PATH = os.path.join(ROOT_DIR, "p95v303b6.win32", "prime.txt")
 P95_TIMEOUT = 60
+
+pyautogui.FAILSAFE = False
 
 def remove_if_exists(filename):
     if os.path.exists(filename):
         os.remove(filename)
 
-remove_if_exists(P95_RESULTS_TXT)
+remove_if_exists(P95_RESULTS_TXT_PATH)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--time', type=int)
@@ -40,7 +42,6 @@ if args.time is not None:
       P95_TIMEOUT = args.time
 
 kill_p95 = subprocess.Popen("taskkill /IM \"prime95.exe\" /F")
-print(kill_p95)
 time.sleep(1)
 p95_process = subprocess.Popen([P95_EXE_PATH, '-t'])
 time.sleep(1)
@@ -65,8 +66,8 @@ time.sleep(P95_TIMEOUT)
 # FindWindow takes the Window Class name (can be None if unknown), and the window's display text. 
 window_handle = FindWindow(None, "Prime95")
 window_rect   = GetWindowRect(window_handle)
-print(window_handle)
-print(window_rect)
+#print(window_handle)
+#print(window_rect)
 x, y, win_x, win_y = window_rect
 pyperclip.copy("")
 ok = windll.user32.BlockInput(True) #enable block
@@ -89,5 +90,5 @@ with open(os.path.join(os.getcwd(), "prime95.log"), "w") as p95_log:
   p95_log_str = p95_log_str.replace("\n", "")
   p95_log.write(p95_log_str)
 
-
-print(ok)
+if os.path.exists(P95_RESULTS_TXT_PATH):
+      shutil.move(P95_RESULTS_TXT_PATH, os.path.join(os.getcwd(), "results.txt"))
